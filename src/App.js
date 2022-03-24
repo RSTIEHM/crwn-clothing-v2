2dc3ac2b94c7564c98fa2241f7d5cc5b27a7f5db
Richard Stiehm <richardstiehm@gmail.com>
2022-03-24 15:26:45 -0400

    Started Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in-component";
+
 
 const Shop = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+
       </Route>
     </Routes>
   );
